211fc752b02e1f1e182beb3c4d2d6296ec8913e3
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-26 13:47:07 +0100

    Header start

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Shoe lazes for smarties!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="Style.css" />
+</head>
+<header>
+<h1>Shoe lazes for smart people</h1>
+</header>
+<body>
+    
