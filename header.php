19fc423910d049c26ec57305fe0abc5b493e15bd
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-26 14:41:27 +0100

    Update

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="Style.css" />
     <link href="https://fonts.googleapis.com/css?family=Krona+One" rel="stylesheet">
+    <script src="script.js"></script>
 </head>
 <body>
     <header>
