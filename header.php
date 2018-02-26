08ba7dc53f865211af5e57ad3ec8d74a1bf481e7
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-26 14:25:48 +0100

    Header uppdatering

--- header.php
+++ header.php
@@ -9,9 +9,29 @@
     <title>Shoe lazes for smarties!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="Style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Krona+One" rel="stylesheet">
 </head>
-<header>
-<h1>Shoe lazes for smart people</h1>
-</header>
 <body>
+    <header>
+        <div>
+            <h1>SHOE LAZES FOR SMART PEOPLE! </h1>
+        <div>
+        <div id="linksdiv">
+            <div>
+                <a href="#">HOME</a>
+            </div>
+            <div>
+                <a href="#">ABOUT US</a>
+            </div>
+            <div>
+                <a href="#">PRODUCTS</a>
+            </div>
+            <div>
+                <a href="#">ADMIN</a>
+            </div>
+            <div>
+                <a href="#">CART</a>
+            </div>
+        </div>
+    </header>
     
